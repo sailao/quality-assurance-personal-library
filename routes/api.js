64a48cf1b84a721669d3d8713f197d4777c35548
sailao <sailaoungseng@gmail.com>
2020-10-04 16:17:16 +0630

    finish issue libary

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,7 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
+const MONGODB_CONNECTION_STRING = process.env.DB || 'mongodb://localhost:27017';
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
@@ -20,15 +20,66 @@ module.exports = function (app) {
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, async(err, db)=>{
+        if (err) throw err;
+        let dbo = db.db('mydb');
+
+        let books = await dbo.collection('books').find().toArray()
+        res.json({
+          code: 200,
+          message: 'success',
+          data: books
+        })
+        db.close()
+      })
     })
     
     .post(function (req, res){
       var title = req.body.title;
+      if(!title){
+        return res.status(400).json({
+          code: 400,
+          message: `invalid params`
+        })
+      }
+      let data = {title: title, commentcount: 0}
       //response will contain new book object including atleast _id and title
+      MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, (err, db)=>{
+        if (err) throw err;
+        let dbo = db.db('mydb');
+
+        dbo.collection('books').insertOne(data, (err, result)=>{
+          if(err) throw err;
+          res.status(201).json({
+            code: 201,
+            message: 'created',
+            data: result.ops[0]
+          })
+          db.close()
+        })
+      })
     })
     
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+      MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, async(err, db)=>{
+        if(err) throw err;
+
+        let dbo = db.db("mydb")
+        let book = await dbo.collection('books').remove({});
+        
+        if(!book.result.ok){
+          return res.status(400).json({
+            code: 400,
+            message: 'cannot delete',
+          })
+        }
+
+        return res.json({
+          code: 200,
+          message: 'complete delete successful'
+        })
+      })
     });
 
 
@@ -36,18 +87,124 @@ module.exports = function (app) {
   app.route('/api/books/:id')
     .get(function (req, res){
       var bookid = req.params.id;
+
+      if(!bookid || !ObjectId.isValid(bookid)){
+        return res.status(400).json({
+          code: 400,
+          message: 'invalid params'
+        })
+      }
+
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, async(err, db)=>{
+        let dbo = db.db("mydb")
+        let books = await dbo.collection('books').aggregate([
+          {
+            $match: {
+              _id: ObjectId(bookid)
+            }
+          },
+          {
+            $project: {
+              _id: {
+                $toString: "$_id"
+              },
+              title: "$title",
+              commentcount: "$commentcount",
+            }
+          },
+          {
+            $lookup: {
+              localField: "_id",
+              from: "comments",
+              foreignField: "bookid",
+              as: "comments"
+            }
+          },
+        ] ).toArray()
+
+        res.status(200).json({
+          code: 200,
+          message: 'success',
+          data: books[0]
+        })
+      })
     })
     
     .post(function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
+      MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, (err, db)=>{
+        if (err) throw err;
+        let dbo = db.db('mydb');
+
+        dbo.collection('comments').insertOne({comment, bookid}, async(err, result)=>{
+          if(err) throw err;
+          let books = await dbo.collection('books').aggregate([
+            {
+              $match: {
+                _id: ObjectId(bookid)
+              }
+            },
+            {
+              $project: {
+                _id: {
+                  $toString: "$_id"
+                },
+                title: "$title",
+                commentcount: "$commentcount",
+              }
+            },
+            {
+              $lookup: {
+                localField: "_id",
+                from: "comments",
+                foreignField: "bookid",
+                as: "comments"
+              }
+            },
+          ] ).toArray()
+          res.status(201).json({
+            code: 201,
+            message: 'success',
+            data: books[0]
+          })
+          db.close()
+        })
+      })
       //json res format same as .get
     })
     
     .delete(function(req, res){
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
+      MongoClient.connect(MONGODB_CONNECTION_STRING, {useUnifiedTopology: true}, async(err, db)=>{
+        if(err) throw err;
+
+        let dbo = db.db("mydb")
+        let book = await dbo.collection('books').findOneAndDelete({_id: ObjectId(bookid)});
+        
+        // if(!book.ok || !book.value.comments || !book.value.comments.length){
+        if(!book.ok){
+          return res.status(400).json({
+            code: 400,
+            message: 'cannot delete book'
+          })
+        }
+        let comments = await dbo.collection('comments').remove({bookid: ObjectId(bookid)});
+
+        if(!comments.result.ok){
+          return res.status(400).json({
+            code: 400,
+            message: 'cannot delete comments'
+          })
+        }
+
+        return res.json({
+          code: 200,
+          message: 'deleted'
+        })
+      })
     });
   
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -19,17 +19,22 @@ suite('Functional Tests', function() {
   * ----[EXAMPLE TEST]----
   * Each test should completely test the response of the API end-point including response status code!
   */
-  test('#example Test GET /api/books', function(done){
-     chai.request(server)
+  test('#example Test GET /api/books', async()=>{
+      (await chai.request(server)
+      .post('/api/books')
+      .send({title: 'new Book'}))
+
+     let queryBook = await chai.request(server)
       .get('/api/books')
-      .end(function(err, res){
-        assert.equal(res.status, 200);
-        assert.isArray(res.body, 'response should be an array');
-        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
-        assert.property(res.body[0], 'title', 'Books in array should contain title');
-        assert.property(res.body[0], '_id', 'Books in array should contain _id');
-        done();
-      });
+      
+      assert.equal(queryBook.status, 200);
+      assert.equal(queryBook.body.code, 200);
+      assert.equal(queryBook.body.message, 'success');
+
+      assert.isArray(queryBook.body.data, 'queryBookponse should be an array');
+      assert.property(queryBook.body.data[0], 'commentcount', 'Books in array should contain commentcount');
+      assert.property(queryBook.body.data[0], 'title', 'Books in array should contain title');
+      assert.property(queryBook.body.data[0], '_id', 'Books in array should contain _id');
   });
   /*
   * ----[END of EXAMPLE TEST]----
@@ -41,33 +46,92 @@ suite('Functional Tests', function() {
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .send({title: 'new Book'})
+        .end((err, result)=>{
+          if(err) done();
+          assert.equal(result.status, 201);
+          assert.equal(result.body.code, 201);
+          assert.equal(result.body.message, 'created');
+          assert.property(result.body.data, '_id')
+          assert.property(result.body.data, 'title')
+          assert.property(result.body.data, 'commentcount')
+          done()
+        })
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+        chai.request(server)
+        .post('/api/books')
+        .end((err, result)=>{
+          if(err) done();
+          assert.equal(result.status, 400);
+          assert.equal(result.body.code, 400);
+          assert.equal(result.body.message, 'invalid params');
+          done()
+        })
       });
       
     });
 
 
     suite('GET /api/books => array of books', function(){
+
       
-      test('Test GET /api/books',  function(done){
-        //done();
+      test('Test GET /api/books',  async()=>{
+        (await chai.request(server).post('/api/books').send({title: 'book 1'}));
+        (await chai.request(server).post('/api/books').send({title: 'book 2'}));
+
+        let getBook = await chai.request(server) .get('/api/books')
+
+        assert.equal(getBook.status, 200);
+        assert.equal(getBook.body.code, 200);
+        assert.equal(getBook.body.message, 'success');
+
+        assert.isArray(getBook.body.data)
+        assert.property(getBook.body.data[0], '_id')
+        assert.property(getBook.body.data[0], 'title')
+        assert.property(getBook.body.data[0], 'commentcount')
+
+        assert.property(getBook.body.data[1], '_id')
+        assert.property(getBook.body.data[1], 'title')
+        assert.property(getBook.body.data[1], 'commentcount')
+
       });      
       
     });
 
 
-    suite('GET /api/books/[id] => book object with [id]', function(){
+    suite('GET /api/books/[id] => book object with [id]', () =>{
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        chai.request(server)
+        .get('/api/books/invalidID')
+        .end((err, result)=>{
+          assert.equal(result.status, 400)
+          assert.equal(result.body.code, 400)
+          assert.equal(result.body.message, 'invalid params')
+          done();
+        })
       });
       
-      test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+      test('Test GET /api/books/[id] with valid id in db',  async()=>{
+        let newBook = await chai.request(server).post('/api/books').send({title: 'new book'});
+        
+        let getBook = await chai.request(server).get(`/api/books/${newBook.body.data._id}`)
+
+        assert.equal(getBook.status, 200)
+        assert.equal(getBook.body.code, 200)
+        assert.equal(getBook.body.message, 'success')
+
+        assert.property(getBook.body.data, '_id')
+        assert.property(getBook.body.data, 'title')
+        assert.property(getBook.body.data, 'commentcount')
+
+        assert.equal(getBook.body.data._id, newBook.body.data._id)
+        assert.equal(getBook.body.data.title, newBook.body.data.title)
+        assert.equal(getBook.body.data.commentcount, 0)
       });
       
     });
@@ -75,8 +139,14 @@ suite('Functional Tests', function() {
 
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
-      test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+      test('Test POST /api/books/[id] with comment', async()=>{
+        let newBook = await chai.request(server).post('/api/books').send({title: 'new book'});
+        
+        let deleteBook = await chai.request(server).delete(`/api/books/${newBook.body.data._id}`)
+
+        assert.equal(deleteBook.status, 200)
+        assert.equal(deleteBook.body.code, 200)
+        assert.equal(deleteBook.body.message, 'deleted')
       });
       
     });
